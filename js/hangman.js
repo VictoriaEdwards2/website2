c272622532bc073b621ed76ed246403d4222b413
Victoria Edwards <142756923+VictoriaEdwards2@users.noreply.github.com>
2024-04-22 17:12:56 +0000

    Mon, Apr 22, 2024, 10:12 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -1,12 +1,12 @@
 const wordEl = document.getElementById('word')
 const wrongLettersEl = document.getElementById('wrong-letters')
-const playAgainBtm = document.getElementById('play-again')
-const popup = document.getElememtById('popup-container')
+const playAgainBtn = document.getElementById('play-again')
+const popup = document.getElementById('popup-container')
 const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 const figureParts = document.querySelectorAll('.figure-part')
 
-const word = ['application', ' programming', 'interface', 'wizard']
+const word = ['HogRider', 'Larry', 'MegaKnight', 'Lebron']
 
 let selectedIndex = Math.floor(word.length * Math.random())
 let selectedWord = word[selectedIndex]
@@ -14,24 +14,32 @@ let selectedWord = word[selectedIndex]
 const correctLetters = []
 const wrongLetters = []
 
-//Show hidden word
-function displayWord()
-{
-	wordEl.innerHTML = `
-	${selectedWord
-.split(' ')
-.map(letter => `
-<span class = "letter">
-	${correctLetters.includes(letter) ? letter : ' '}
-</span>
-`)join(' ')
- }
-`
- const innerWord = wordEl.innerText.replace(/\n/g, '')
+//show hidden word
+function displayWord() {
+    wordEl.innerHTML = `
+        ${selectedWord
+            .split('')
+            .map(letter => `
+                <span class="letter">
+                    ${correctLetters.includes(letter) ? letter : ''}
+                </span>
+            `).join('')
+        }
 
- if(innerWord == selectedWord) {
-    finalMessage.innerText = 'Congratulations! You Won!'
-    popup.style.display = 'flex'
- }
+    `
+    const innerWord = wordEl.innerText.replace(/\n/g, '')
+
+    if (innerWord == selectedWord) {
+        finalmessage.innerText = 'Congratulations! You Won!'
+        popup.style.display = 'flex'
+    }
 }
+
+displayWord()
+
+
+
+
+//Keydown letter press
+window.addEventListner('keydown')
 displayWord()
