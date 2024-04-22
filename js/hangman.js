1587448a334d8dee229172487f03ed64a8435a97
Victoria Edwards <142756923+VictoriaEdwards2@users.noreply.github.com>
2024-04-22 17:01:27 +0000

    Mon, Apr 22, 2024, 10:01 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -1,12 +1,12 @@
 const wordEl = document.getElementById('word')
-wrongLettersEl = document.getElementById('wrong-letters')
+const wrongLettersEl = document.getElementById('wrong-letters')
 const playAgainBtm = document.getElementById('play-again')
 const popup = document.getElememtById('popup-container')
 const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 const figureParts = document.querySelectorAll('.figure-part')
 
-const word = ['application', ' programming', 'interface', ‘wizard’]
+const word = ['application', ' programming', 'interface', 'wizard']
 
 let selectedIndex = Math.floor(word.length * Math.random())
 let selectedWord = word[selectedIndex]
