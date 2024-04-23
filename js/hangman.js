d04ccb353ba7c987d1e80d8b9d5d1bcc80083da8
Victoria Edwards <142756923+VictoriaEdwards2@users.noreply.github.com>
2024-04-23 16:47:44 +0000

    Tue, Apr 23, 2024, 9:47 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -28,7 +28,7 @@ function displayWord() {
 
     `
 
-    if (innerWord == selectedWord) {
+    if(innerWord == selectedWord) {
         finalmessage.innerText = 'Congratulations! You Won!'
         popup.style.display = 'flex'
     }
@@ -38,7 +38,21 @@ function displayWord() {
 function updateWrongLetterEl()
 {
     wrongLettersEl.innerHTML = `
-    $
+    ${wrongLetters.length > 0 ? '<p>Wrong<p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `
+
+    figureParts.forEach((part, index) =>
+    {
+        const errors = wrongLetters.length
+        if(index < errors)
+        {
+            part.style.display = 'block'
+        } else
+        {
+            part.style.display = 'none'
+        }
+    })
 }
 
 // show notification
